b780ad83d45987a27f29bd1506f882cd48fa44c7
BrianGoodman95 <40720524+BrianGoodman95@users.noreply.github.com>
2020-06-16 23:01:51 -0400

    Re-organized file structure

--- ParsingTools/Setup/Directory_setup.py
+++ ParsingTools/Setup/Directory_setup.py
@@ -3,11 +3,17 @@ import os
 
 class Create_Directories():
     def __init__(self):
-        self.project_path = pathlib.Path().absolute()
+        dir_path = pathlib.Path().absolute()
+        Data_Path = "Data"
+        self.project_path = f'{str(dir_path)}\{Data_Path}'
+        try:
+            os.mkdir(Data_Path)
+        except FileExistsError:
+            pass
         self.Data_folders = ['Raw Data', 'Raw Data/WDVOA DATA', 'Total Data', 'Total Data/Evaluations', 'ML Data', 'ML Data/Model Data',  'ML Data/Prediction Data',  'ML Data/Train_Test_Data']
         for folder_path in self.Data_folders:
             try:
-                os.mkdir(folder_path)
+                os.mkdir(f'{Data_Path}/{folder_path}')
                 print(f'Created Directory {folder_path}')
             except FileExistsError:
                 print(f'Directory {folder_path} already exists')
